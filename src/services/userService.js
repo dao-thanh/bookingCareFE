acd515b056cd919b197d8c3b227a70b7a57a6da0
Benny-x21 <benny@enclave.vn>
2022-09-26 11:47:07 +0700

    show patient list for scheduled with doctor

--- src/services/userService.js
+++ src/services/userService.js
@@ -92,9 +92,13 @@ const createNewClinic = (data) => {
     return axios.post(`/api/create-new-clinic`, data)
 }
 
+const getAllPatientForDoctor = (data) => {
+    return axios.get(`/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`)
+}
+
 export {handleLoginApi,getAllUsers, createNewUserService, deleteUserService, 
     editUserService, getAllCodeService, getTopDoctorHomeService, getAllDoctors,saveDetailDoctor,
     getDetailInforDoctor, saveBulkScheduleDoctor, getScheduleDoctorByDate, getExtraInforDoctorById,
     getProfileDoctorById, postPatientBookAppointment,postVerifyBookAppointment,createNewSpecialty, getAllSpecialty, 
-    getAllDetailSpecialtyById, createNewClinic, getAllClinic, getAllDetailClinicById }
+    getAllDetailSpecialtyById, createNewClinic, getAllClinic, getAllDetailClinicById, getAllPatientForDoctor}
  
